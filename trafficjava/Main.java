d5a18a62e287ba4fea07af22e994844c1bf7e7df
Gugert546 <halstensen55@gmail.com>
2025-03-25 18:00:31 +0100

    fungerede vei generation i vei.java, halvveis oppbygd bane i main.java. oppdatert og ryddig cross.java

--- trafficjava/Main.java
+++ trafficjava/Main.java
@@ -1,7 +1,6 @@
 package trafficjava;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -9,14 +8,39 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     public void start(Stage primaryStage) {
-        cross cross = new cross(50, 500, true, true, false, false);
-        cross cross2 = new cross(50, 20, false, true, true, false);
-        cross cross3 = new cross(400, 20, false, true, true, true);
-        cross cross4 = new cross(720, 20, false, false, true, true);
-        vei vei1 = new vei(true, 50, 40, 400);
-        vei vei2 = new vei(true, 400, 40, 400);
+        Pane TrafficPane = new Pane();
 
-        Pane TrafficPane = new Pane(cross, cross2, vei1, cross3, cross4, vei2);
+        cross cross = new cross(50, 550, true, true, false, false);// nederst til venstre
+        cross cross2 = new cross(50, 50, false, true, true, false);// øverst til venstre
+        cross cross7 = new cross(50, 300, true, true, true, false);// midten til venstre
+        TrafficPane.getChildren().addAll(cross, cross2, cross7);
+        cross cross3 = new cross(400, 50, false, true, true, true);// midten oppe
+        cross crossbm = new cross(400, 550, true, true, false, true);// midten nede
+        cross midten = new cross(400, 300, true, true, true, true); // i midten
+        TrafficPane.getChildren().addAll(cross3, crossbm, midten);
+        cross cross4 = new cross(750, 50, false, false, true, true); // øverst til høyre
+        cross cross5 = new cross(750, 550, true, false, false, true); // nederst til høyre
+        cross cross6 = new cross(750, 300, true, false, true, true); // midten til høyre
+        TrafficPane.getChildren().addAll(cross4, cross5, cross6);
+
+        // vertikale veier
+        vei vei1 = new vei(true, cross2.getX(), cross2.getY(), cross7.getY());
+        vei vei2 = new vei(true, cross3.getX(), cross3.getY(), midten.getY());
+        vei vei3 = new vei(true, cross4.getX(), cross4.getY(), cross6.getY());
+        vei vei4 = new vei(true, cross7.getX(), cross7.getY(), cross.getY());
+        vei vei5 = new vei(true, midten.getX(), midten.getY(), crossbm.getY());
+        vei vei6 = new vei(true, cross6.getX(), cross6.getY(), cross5.getY());
+        TrafficPane.getChildren().addAll(vei1, vei2, vei3, vei4, vei5, vei6);
+
+        // horisontale veier
+        vei vei7 = new vei(false, cross2.getX(), cross2.getY(), cross3.getX());
+        vei vei8 = new vei(false, cross7.getX(), cross7.getY(), midten.getX());
+        vei vei9 = new vei(false, cross.getX(), cross.getY(), crossbm.getX());
+        vei vei10 = new vei(false, cross3.getX(), cross3.getY(), cross4.getX());
+        vei vei11 = new vei(false, midten.getX(), midten.getY(), cross6.getX());
+        vei vei12 = new vei(false, crossbm.getX(), crossbm.getY(), cross5.getX());
+        TrafficPane.getChildren().addAll(vei7, vei8, vei9, vei10, vei11, vei12);
+
         Scene trafficSim = new Scene(TrafficPane, 800, 600);
         primaryStage.setTitle("traffic sim");
         primaryStage.setScene(trafficSim);
--- trafficjava/cross.java
+++ trafficjava/cross.java
@@ -1,47 +1,54 @@
 package trafficjava;
 
 import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
 
 public class cross extends Group {
 
     Pane gui = new Pane();
-    int lengde = 120;
-    private static int bredde = 80;
+    int lengde = 60;
+    private static int bredde = 40;
+    int x;
+    int y;
 
-    public cross(int x, int y, boolean opp, boolean høyre, boolean ned, boolean venstre) {
-
+    /**
+     * konsturktør for kryss
+     * 
+     * @param x       start x posisjon, midt i krysset
+     * @param y       start y posisjon,midt i krysset
+     * @param opp     false/true for å ha med denne siden av krysset
+     * @param høyre   false/true for å ha med denne siden av krysset
+     * @param ned     false/true for å ha med denne siden av krysset
+     * @param venstre false/true for å ha med denne siden av krysset
+     * 
+     */
+    public cross(int midtX, int midtY, boolean opp, boolean høyre, boolean ned, boolean venstre) {
+        this.x = midtX;
+        this.y = midtY;
         // varibler for roadmarkings
-        int middlex = x;
-        int middley = y + bredde / 2;
-        int ox = x - bredde / 2;
-        int ny = y + bredde / 2;
-        int vx = x - lengde;
-        // variabler linje y akse
-        int ey = y + (lengde + bredde / 2);
-        int oy = y - (lengde - bredde / 2);
-
-        // variabler linje x akse
-        int sx = x - lengde;
-        int ex = x + lengde;
+        int halvbredde = bredde / 2;
+        int xtilvenstre = x - halvbredde;
+        int yoppover = y - halvbredde;
+        int xlangttilvenstre = x - lengde;
+        int ylangtoppover = y - lengde;
 
         // grå boks for å skjule midten
-        int bx = x - bredde / 2;
 
-        Rectangle box = new Rectangle(bx, y, bredde, bredde);
+        Rectangle box = new Rectangle(xtilvenstre, yoppover, bredde, bredde);
         box.setFill(Color.GRAY);
         getChildren().addAll(box);
+
         // opp
+        // x må stå -halvparten av bredden til venstre
         if (opp == true) {
-            Rectangle roadR3 = new Rectangle(ox, oy, bredde, lengde);
+
+            Rectangle roadR3 = new Rectangle(xtilvenstre, ylangtoppover, bredde, lengde);
             roadR3.setFill(Color.GRAY);
             getChildren().addAll(roadR3);
-            Line mopp = new Line(middlex, middley, middlex, oy);
+            Line mopp = new Line(x, y, x, ylangtoppover);
             mopp.getStrokeDashArray().addAll(11d);
             mopp.setStroke(Color.YELLOW);
             mopp.setStrokeWidth(bredde / 20);
@@ -52,11 +59,13 @@ public class cross extends Group {
              */
         }
         // høyre
+        // y må stå oppover
         if (høyre == true) {
-            Rectangle roadR1 = new Rectangle(x, y, lengde, bredde);
+            int endx = x + lengde;
+            Rectangle roadR1 = new Rectangle(x, yoppover, lengde, bredde);
             roadR1.setFill(Color.GRAY);
             getChildren().addAll(roadR1);
-            Line mhøyre = new Line(middlex, middley, ex, middley);
+            Line mhøyre = new Line(x, y, endx, y);
             mhøyre.getStrokeDashArray().addAll(11d);
             mhøyre.setStroke(Color.YELLOW);
             mhøyre.setStrokeWidth(bredde / 20);
@@ -66,11 +75,13 @@ public class cross extends Group {
              */
         }
         // ned
+        // x må stå til venstre
         if (ned == true) {
-            Rectangle roadR4 = new Rectangle(ox, ny, bredde, lengde);
+            int endy = y + lengde;
+            Rectangle roadR4 = new Rectangle(xtilvenstre, y, bredde, lengde);
             roadR4.setFill(Color.GRAY);
             getChildren().addAll(roadR4);
-            Line mned = new Line(middlex, middley, middlex, ey);
+            Line mned = new Line(x, y, x, endy);
             mned.getStrokeDashArray().addAll(11d);
             mned.setStroke(Color.YELLOW);
             mned.setStrokeWidth(bredde / 20);
@@ -80,11 +91,12 @@ public class cross extends Group {
              */
         }
         // venstre
+        // x må stå langt il venstre og y oppover
         if (venstre == true) {
-            Rectangle roadR2 = new Rectangle(vx, y, lengde, bredde);
+            Rectangle roadR2 = new Rectangle(xlangttilvenstre, yoppover, lengde, bredde);
             roadR2.setFill(Color.GRAY);
             getChildren().addAll(roadR2);
-            Line mvenstre = new Line(middlex, middley, sx, middley);
+            Line mvenstre = new Line(x, y, xlangttilvenstre, y);
             mvenstre.getStrokeDashArray().addAll(11d);
             mvenstre.setStroke(Color.YELLOW);
             mvenstre.setStrokeWidth(bredde / 20);
@@ -96,109 +108,20 @@ public class cross extends Group {
 
     }
 
-    /**
-     * metode for å lage et veikryss, må skrive true/false for å bestemme hvilke
-     * retninger det skal ha
-     */
-    public Group addCross(int x, int y, boolean opp, boolean høyre, boolean ned, boolean venstre) {
-        Group cross = new Group();
-        // varibler for roadmarkings
-        int middlex = x;
-        int middley = y + bredde / 2;
-        int ox = x - bredde / 2;
-        int ny = y + bredde / 2;
-        int vx = x - lengde;
-        // variabler linje y akse
-        int ey = y + (lengde + bredde / 2);
-        int oy = y - (lengde - bredde / 2);
+    /** metode for å få x posisjonen til krysset(midt i) */
+    public int getX() {
+        return x;
 
-        // variabler linje x akse
-        int sx = x - lengde;
-        int ex = x + lengde;
-
-        // grå boks for å skjule midten
-        int bx = x - bredde / 2;
-
-        Rectangle box = new Rectangle(bx, y, bredde, bredde);
-        box.setFill(Color.GRAY);
-        cross.getChildren().addAll(box);
-        // opp
-        if (opp == true) {
-            Rectangle roadR3 = new Rectangle(ox, oy, bredde, lengde);
-            roadR3.setFill(Color.GRAY);
-            cross.getChildren().addAll(roadR3);
-            Line mopp = new Line(middlex, middley, middlex, oy);
-            mopp.getStrokeDashArray().addAll(11d);
-            mopp.setStroke(Color.YELLOW);
-            mopp.setStrokeWidth(bredde / 20);
-            cross.getChildren().addAll(mopp);
-
-            /*
-             * trafficLight.addLight()
-             */
-        }
-        // høyre
-        if (høyre == true) {
-            Rectangle roadR1 = new Rectangle(x, y, lengde, bredde);
-            roadR1.setFill(Color.GRAY);
-            cross.getChildren().addAll(roadR1);
-            Line mhøyre = new Line(middlex, middley, ex, middley);
-            mhøyre.getStrokeDashArray().addAll(11d);
-            mhøyre.setStroke(Color.YELLOW);
-            mhøyre.setStrokeWidth(bredde / 20);
-            cross.getChildren().addAll(mhøyre);
-            /*
-             * trafficLight.addLight()
-             */
-        }
-        // ned
-        if (ned == true) {
-            Rectangle roadR4 = new Rectangle(ox, ny, bredde, lengde);
-            roadR4.setFill(Color.GRAY);
-            cross.getChildren().addAll(roadR4);
-            Line mned = new Line(middlex, middley, middlex, ey);
-            mned.getStrokeDashArray().addAll(11d);
-            mned.setStroke(Color.YELLOW);
-            mned.setStrokeWidth(bredde / 20);
-            cross.getChildren().addAll(mned);
-            /*
-             * trafficLight.addLight()
-             */
-        }
-        // venstre
-        if (venstre == true) {
-            Rectangle roadR2 = new Rectangle(vx, y, lengde, bredde);
-            roadR2.setFill(Color.GRAY);
-            cross.getChildren().addAll(roadR2);
-            Line mvenstre = new Line(middlex, middley, sx, middley);
-            mvenstre.getStrokeDashArray().addAll(11d);
-            mvenstre.setStroke(Color.YELLOW);
-            mvenstre.setStrokeWidth(bredde / 20);
-            cross.getChildren().addAll(mvenstre);
-            /*
-             * trafficLight.addLight()
-             */
-        }
-        return cross;
     }
 
-    /** metode for å lage en vertikal vei */
-
-    /** legger et trådkors på skjermen for å visualisere plasseringer */
-    public void addCoordinates() {
-        Line xakse = new Line(0, 300, 800, 300);
-        xakse.setStroke(Color.BLACK);
-        xakse.setStrokeWidth(10);
-        Line yakse = new Line(400, 0, 400, 600);
-        yakse.setStroke(Color.BLACK);
-        yakse.setStrokeWidth(10);
-
-        gui.getChildren().addAll(xakse, yakse);
+    /** metode for å få y posisjonen til krysset(midt i) */
+    public int getY() {
+        return y;
     }
 
     /** metode for å sette bredden på veien/krysset */
     public void setBredde(int bredde) {
-        this.bredde = bredde;
+        cross.bredde = bredde;
     }
 
     /** metode for å få bredden på veien/veien i krysset */
